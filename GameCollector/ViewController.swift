0426c1ac9b66bff386058e3c53e2cf254ca34a2e
the Luckiest <m02yyd@gmail.com>
2017-06-10 02:28:46 +0300

    We create game objects and pull them out from core data

--- GameCollector/ViewController.swift
+++ GameCollector/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var games: [Game] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            try games = context.fetch(Game.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let game = games[indexPath.row]
+        let cell = UITableViewCell()
+        cell.textLabel?.text = game.title
+        cell.imageView?.image = UIImage(data: game.image! as Data)
+        return cell
+    }
 
 }
 
